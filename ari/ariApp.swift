810b8d1c5702281c081787b0cbbf9d593a6726dd
arinltte <166289099+arinltte@users.noreply.github.com>
2026-05-22 17:43:41 +0800

    Initial Commit

---
+++ ari/ariApp.swift
@@ -0,0 +1,17 @@
+//
+//  ariApp.swift
+//  ari
+//
+//  Created by Chen Jin Shen on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
